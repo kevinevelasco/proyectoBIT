077a978b0f38659c4ef41fe5ecb77fcb264eaff0
KevinVel16 <90013072+KevinVel16@users.noreply.github.com>
2023-05-26 19:00:25 -0500

    commit inicial

---
+++ src/components/Home/home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const home = () => {
+  return ( //para tener más de un componente se genera un contenedor con <>
+  <>
+  
+     <div>home</div>
+  </>
+ 
+  )
+}
+
+export default home
